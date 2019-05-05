712c0a97b6129ee3c34c60b40273d68e8906f4c1
daschne8 <daschne8@gmail.com>
2019-05-05 00:50:02 -0400

    switch comp

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,22 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories, reject_if: proc{|attributes|
+    attributes.all? do |key,value|
+      if value.is_a? ActionController::Parameters
+        value.all? {|nested_key, nested_value| nested_key == '_destroy' || nested_value.blank?}
+      else
+        key == '_destory' || value.blank?
+      end
+    end
+  }
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+p1 = Post.create(title: "title 1", content: "1 content here")
+cat1 = Category.create(name: "1 Super Fly")
+com1 = Comment.create(content: "1 is fun")
+usr1 = User.create(username: "Charlie")
+usr1.comments << com1
+p1.categories << cat1
+p1.comments << com1
+p1.save
+usr1.save
